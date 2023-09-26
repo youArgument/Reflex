eea3d10fb2c3ce80f670d97428a26bf956f48fa7
Eduard <99264101+youArgument@users.noreply.github.com>
2023-09-26 19:42:23 +0300

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -1,16 +1,9 @@
 import reflex as rx
-import os
 
-railway_domain = "RAILWAY_PUBLIC_DOMAIN"
-
-class ReflextemplateConfig(rx.Config):
+class ReflextestConfig(rx.Config):
     pass
 
-config = ReflextemplateConfig(
-    app_name="myapp",
-    telemetry_enabled=False,
-    frontend_port=3000, # default frontend port
-    backend_port=8000, # default backend port
-    # use https and the railway public domain with a backend route if available, otherwise default to a local address
-    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
+config = ReflextestConfig(
+    app_name="ReflexTest",
+    api_url="weseebot.ru:8000"
 )
